12fd2a7d0b63fefe27c6b553d6deb261653c2e29
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-08-09 12:33:26 +0200

    Implement binary search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class BinarySearch {
+    public boolean isNumberOnList(int searchNumber, List<Integer> list){
+        try{
+            search(searchNumber, list);
+        } catch (NoSuchElementException ex){
+            return false;
+        }
+        return true;
+    }
+
+    private int search(int searchNumber, List<Integer> list){
+        if(list.size() == 0){
+            throw new NoSuchElementException();
+        }
+
+        int medium = list.size()/2;
+        int mediumValue = list.get(medium);
+
+        if(list.get(medium) == searchNumber){
+            return medium;
+        }
+
+        if(list.size() <= 1){
+            throw new NoSuchElementException();
+        }
+
+        if(mediumValue < searchNumber){
+            List<Integer> rightList = new ArrayList<>(list.subList(medium + 1, list.size()));
+            return search(searchNumber, rightList);
+        }else{
+            List<Integer> leftList = new ArrayList<>(list.subList(0, medium));
+            return search(searchNumber, leftList);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        BinarySearch binarySearch = new BinarySearch();
+
+        System.out.println("Search 2 in list: 1, 2, 3, 4, 5, 6, 7");
+        boolean wasFound = binarySearch.isNumberOnList(1000, Arrays.asList(-4,2,3,4,5, 1000));
+        System.out.println("Was found? - " + wasFound);
+    }
 }
